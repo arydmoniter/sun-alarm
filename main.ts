d2421c4035db93fa965c23b2613567d2de209704
arydmoniter <161125037+arydmoniter@users.noreply.github.com>
2024-05-25 13:42:41 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 let result = 0
+let uvl = 0
 basic.clearScreen()
 basic.forever(function () {
-    result = pins.analogReadPin(AnalogPin.P1)
-    result = result / 100
+    uvl = pins.analogReadPin(AnalogPin.P1)
+    result = uvl / 100
+    basic.pause(100)
     if (result >= 5) {
         music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         serial.writeValue("value", result)
