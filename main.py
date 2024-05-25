cee73b9cbbd2055748bc75edabd501d9b3ae6b0f
arydmoniter <161125037+arydmoniter@users.noreply.github.com>
2024-05-25 13:58:40 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-basic.clear_screen()
+uvl = 0
+result = 0
 
 def on_forever():
-    result = pins.analog_read_pin(AnalogPin.P0)
-    result = result/10
-    basic.show_number(result)
+    global uvl, result
+    uvl = pins.analog_read_pin(AnalogPin.P1)
+    result = uvl / 1
+    basic.pause(100)
     serial.write_value("value", result)
+    if result >= 3:
+        music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+            music.PlaybackMode.UNTIL_DONE)
+        basic.pause(1000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
-let result = 0
 let uvl = 0
-basic.clearScreen()
+let result = 0
 basic.forever(function () {
     uvl = pins.analogReadPin(AnalogPin.P1)
-    result = uvl / 100
+    result = uvl / 10
     basic.pause(100)
-    if (result >= 5) {
+    serial.writeValue("value", result)
+    if (result >= 3) {
         music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        serial.writeValue("value", result)
         basic.pause(1000)
     }
+    basic.showNumber(result)
 })
